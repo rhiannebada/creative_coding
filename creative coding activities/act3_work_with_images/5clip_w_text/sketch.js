0be1dfd05111bf6f93b91cd0ba9b4e0359eaf1cf
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2024-06-11 06:48:31 +0400

    Update sketch.js

--- creative coding activities/act3_work_with_images/5clip_w_text/sketch.js
+++ creative coding activities/act3_work_with_images/5clip_w_text/sketch.js
@@ -12,13 +12,19 @@ function setup() {
 
   let maskGraphics = createGraphics(500, 500);
   maskGraphics.fill(0); 
-  maskGraphics.textSize(90); 
-  maskGraphics.textAlign(CENTER, CENTER);
-  maskGraphics.textStyle(BOLDITALIC); 
+
+  // Text properties
+  maskGraphics.textSize(90); // text size
+  maskGraphics.textAlign(CENTER, CENTER); // center align
+  maskGraphics.textStyle(BOLDITALIC); // bold italic text style
+
+  // Draw text
   maskGraphics.text('CREATIVE', maskGraphics.width / 2, maskGraphics.height / 3 + 50); 
   maskGraphics.text('CODING', maskGraphics.width / 2, maskGraphics.height * 2 / 3 - 30); 
   
+  // Apply mask to image
   img.mask(maskGraphics);
 
+  // Draw masked image at the center
   image(img, (width - img.width) / 2, (height - img.height) / 2);
 }

2cac66a955d801ef56e45ebb2f6e537443ceb037
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2024-06-11 06:48:36 +0400

    Update sketch.js

--- creative coding activities/act3_work_with_images/3posterize_filter/sketch.js
+++ creative coding activities/act3_work_with_images/3posterize_filter/sketch.js
@@ -10,7 +10,7 @@ function setup() {
 
 function draw() {
   background(0);
-  img.resize(400, 400);
+  img.resize(400, 400); // Resize image
   image(img, 0, 0); 
-  filter(POSTERIZE, 2); 
+  filter(POSTERIZE, 2); // apply posterize filter with a level of 2
 }

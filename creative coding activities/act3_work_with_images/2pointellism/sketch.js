e39f5e22e92fd010e1416825dd5bde343361bc27
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2024-06-11 06:48:38 +0400

    Update sketch.js

--- creative coding activities/act3_work_with_images/2pointellism/sketch.js
+++ creative coding activities/act3_work_with_images/2pointellism/sketch.js
@@ -1,38 +1,19 @@
 var img, x, y;
-
 function preload() {
-
 img = loadImage("blossoms.jpeg");
-
 }
-
-
 
 function setup() {
-
 createCanvas (600, 400);
-
 background(0);
-
 noStroke();
-
 }
-
-
 
 function draw() {
-
 x = random(width);
-
 y = random(height);
-
-var c = img.get(x, y);
-
+var c = img.get(x, y); // get pixel color at the random position from the image
 fill(c[0], c[1], c[2], 100); //increase transparency
-
-ellipse (x, y, 12, 25);
-
+ellipse (x, y, 12, 25); // draw ellipse st the rsndom podition
 img.resize(600, 400);
-
 }
-

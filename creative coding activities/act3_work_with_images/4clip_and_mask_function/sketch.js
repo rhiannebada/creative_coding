03bc6d59fbff1e823e3f0589943bbc782aa80f6b
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2024-06-11 06:48:33 +0400

    Update sketch.js

--- creative coding activities/act3_work_with_images/4clip_and_mask_function/sketch.js
+++ creative coding activities/act3_work_with_images/4clip_and_mask_function/sketch.js
@@ -11,15 +11,17 @@ function setup() {
   img.resize(500, 500);
   let maskGraphics = createGraphics(500, 500);
   maskGraphics.fill(255);
-  maskGraphics.circle(100, 150, 190);
-  maskGraphics.circle(400, 150, 190);
-  maskGraphics.circle(250, 300, 300);
-  
-  img.mask(maskGraphics);
 
-  let x = (width - img.width) / 2;
+  // Circles
+  maskGraphics.circle(100, 150, 190); // left ear
+  maskGraphics.circle(400, 150, 190); // right ear
+  maskGraphics.circle(250, 300, 300); // face
+  
+  img.mask(maskGraphics); // apply mask tot he image
+
+  // Position to center image
+  let x = (width - img.width) / 2; 
   let y = (height - img.height) / 2;
   
   image(img, x, y);
-  
 }
